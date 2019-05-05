e8f608f2bf5f07c86973da32f679f959a8e25cf4
Lars-Kristian Dahl <lars-kristian@hoopit.io>
2019-05-05 23:30:22 +0200

    Add bus filter and option to disable departure sensors

--- homeassistant/components/atb/sensor.py
+++ homeassistant/components/atb/sensor.py
@@ -22,6 +22,7 @@ ATTRIBUTION = "Data provided by https://atbapi.tar.io/"
 CONF_STOP_ID = 'stop_id'
 CONF_STOP_NAME = 'stop_name'
 CONF_BUS_FILTER = 'bus_filter'
+CONF_DEPARTURE_SENSORS = 'all_departures'
 
 TIMEZONE = "Europe/Oslo"
 tz = pytz.timezone(TIMEZONE)
@@ -34,6 +35,7 @@ SENSOR_SCHEMA = vol.Schema({
     vol.Required(CONF_STOP_ID): cv.string,
     vol.Optional(CONF_STOP_NAME): cv.string,
     vol.Optional(CONF_BUS_FILTER): cv.ensure_list_csv,
+    vol.Optional(CONF_DEPARTURE_SENSORS, default=False): cv.boolean,
 })
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
@@ -49,7 +51,9 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     for device, device_config in config[CONF_SENSORS].items():
         stop = device_config[CONF_STOP_ID]
         stop_name = device_config.get(CONF_STOP_NAME)
-        stop_sensor = AtbStopSensor(hass, device, stop, stop_name)
+        bus_filter = device_config.get(CONF_BUS_FILTER)
+        departure_sensors = device_config.get(CONF_DEPARTURE_SENSORS)
+        stop_sensor = AtbStopSensor(hass, device, stop, stop_name, bus_filter, departure_sensors)
         sensors.append(stop_sensor)
         sensors.extend(stop_sensor.departure_sensors)
 
@@ -75,15 +79,17 @@ class AtbStopSensor(Entity):
     The state is the time of the next expected departure.
     """
 
-    def __init__(self, hass, device, stop_id, stop_name):
+    def __init__(self, hass, device: str, stop_id: int, stop_name: str, bus_filter: List[str], departure_sensors: bool):
         self._data = {}
         self._state = STATE_UNKNOWN
         self._stop_id = stop_id
         self._name = stop_name
+        self._bus_filter = set(bus_filter) if bus_filter else None
         self.entity_id = generate_entity_id(ENTITY_ID_FORMAT, device, hass=hass)
         self.departure_sensors: List[AtbDepartureSensor] = []
-        for i in range(0, 5):
-            self.departure_sensors.append(AtbDepartureSensor(hass, f"{device}_{i}"))
+        if departure_sensors:
+            for i in range(0, 5):
+                self.departure_sensors.append(AtbDepartureSensor(hass, f"{device}_{i}"))
 
     @property
     def name(self):
@@ -113,7 +119,12 @@ class AtbStopSensor(Entity):
     def update(self):
         """Update device state."""
         self._data = requests.get(f"{_RESOURCE}{self._stop_id}/").json()
-        self._state = parse_datetime(self._data['departures'][0]['registeredDepartureTime'])
+        self._state = STATE_UNKNOWN
+        for departure in self._data['departures']:
+            if not self._bus_filter or int(departure['line']) in self._bus_filter:
+                self._state = parse_datetime(departure['registeredDepartureTime'])
+                break
+        # self._state = parse_datetime(self._data['departures'][0]['registeredDepartureTime'])
         length = min(len(self._data['departures']), len(self.departure_sensors))
         for index in range(0, length):
             self.departure_sensors[index].update_sensor(self._data['departures'][index])
